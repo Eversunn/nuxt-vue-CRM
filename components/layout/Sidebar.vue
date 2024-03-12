602b5ae993bbd7af451e87ae30974aba1a0befc9
Timofei Sliudikov <timofeislyudikov@gmail.com>
2024-03-12 15:20:47 +0600

    1

---
+++ components/layout/Sidebar.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+<aside>
+    <NuxtLink to="/">
+        <NuxtImg src="/logo.svg" alt="logo" width="100px" />
+    </NuxtLink>
+</aside>
+</template>
+
+
+<style scoped>
+</style>
